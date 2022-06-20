05785babea8d3a6bb1e2fb6915efd3059faa1018
AndrewJacop <73601700+AndrewJacop@users.noreply.github.com>
2022-06-20 15:40:06 +0200

    updated UI for answers

--- lib/main.dart
+++ lib/main.dart
@@ -30,10 +30,10 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(title: const Text('Quiz App')),
       body: PageView.builder(
-        physics: NeverScrollableScrollPhysics(),
+        physics: const NeverScrollableScrollPhysics(),
         itemCount: questions.length,
         itemBuilder: (context, i) {
-          final _question = questions[i];
+          final question = questions[i];
           return Padding(
             padding: const EdgeInsets.all(12),
             child: Column(
@@ -43,9 +43,21 @@ class _HomeScreenState extends State<HomeScreen> {
                 Text('Question $i / ${questions.length}',
                     style: const TextStyle(fontSize: 30)),
                 const Divider(thickness: 3),
-                Text(_question.txt, style: const TextStyle(fontSize: 20)),
+                Text(question.txt, style: const TextStyle(fontSize: 20)),
                 const SizedBox(height: 5),
-                Expanded(child: OptWdg(question: _question))
+                Expanded(
+                    child: OptionWidget(
+                        question: question,
+                        onClickedOption: (option) {
+                          if (question.isLocked) {
+                            return;
+                          } else {
+                            setState(() {
+                              question.isLocked = true;
+                              question.selectedOption = option;
+                            });
+                          }
+                        }))
               ],
             ),
           );
@@ -55,38 +67,52 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class OptWdg extends StatelessWidget {
+class OptionWidget extends StatelessWidget {
   final Question question;
-  const OptWdg({Key? key, required this.question}) : super(key: key);
+  final ValueChanged<Option> onClickedOption;
+  const OptionWidget(
+      {Key? key, required this.question, required this.onClickedOption})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children:
-          question.options.map((option) => bldOpt(context, option)).toList(),
+      children: question.options
+          .map((option) => buildOption(context, option))
+          .toList(),
     );
   }
-}
 
-Widget bldOpt(BuildContext context, Option option) {
-  return GestureDetector(
-    onTap: () => onClickOption(option),
-    child: Container(
-      height: 50,
-      padding: EdgeInsets.all(12),
-      margin: EdgeInsets.symmetric(vertical: 8),
-      decoration: BoxDecoration(
-        color: Colors.grey[300],
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(15),
+  Widget buildOption(BuildContext context, Option option) {
+    final borderColor = getColorForOption(option, question);
+    return GestureDetector(
+      onTap: () => onClickedOption(option),
+      child: Container(
+        height: 50,
+        padding: EdgeInsets.all(12),
+        margin: EdgeInsets.symmetric(vertical: 8),
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+          border: Border.all(color: borderColor, width: 1.5),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Row(
+          children: [
+            Text(option.txt, style: TextStyle(fontSize: 20)),
+          ],
+        ),
       ),
-      child: Row(
-        children: [
-          Text(option.opt, style: TextStyle(fontSize: 20)),
-        ],
-      ),
-    ),
-  );
-}
+    );
+  }
 
-onClickOption(Option option) {}
+  Color getColorForOption(Option option, Question question) {
+    if (question.isLocked) {
+      if (option == question.selectedOption) {
+        return option.isCorrect ? Colors.green : Colors.red;
+      } else if (option.isCorrect) {
+        return Colors.green;
+      }
+    }
+    return Colors.grey;
+  }
+}
--- lib/model.dart
+++ lib/model.dart
@@ -1,9 +1,9 @@
 class Option {
-  String opt;
+  String txt;
   bool isCorrect;
 
   Option({
-    required this.opt,
+    required this.txt,
     required this.isCorrect,
   });
 }
@@ -12,6 +12,7 @@ class Question {
   String txt;
   List<Option> options;
   bool isLocked;
+  Option? selectedOption;
 
   Question({
     required this.txt,
@@ -24,91 +25,91 @@ final questions = [
   Question(
       txt: 'Which one of the following is non-crystalline or amorphous?',
       options: [
-        Option(opt: 'Diamond', isCorrect: false),
-        Option(opt: 'Graphite', isCorrect: false),
-        Option(opt: 'Glass', isCorrect: true),
-        Option(opt: 'Common Salt', isCorrect: false),
+        Option(txt: 'Diamond', isCorrect: false),
+        Option(txt: 'Graphite', isCorrect: false),
+        Option(txt: 'Glass', isCorrect: true),
+        Option(txt: 'Common Salt', isCorrect: false),
       ]),
   Question(
       txt:
           'NaCl typecrystal (with coordination no. 6 : 6) can be converted into CsCl type crystal (with coordination no. 8 : 8) by applying',
       options: [
-        Option(opt: 'high temperature', isCorrect: false),
-        Option(opt: 'high pressure', isCorrect: true),
-        Option(opt: 'high temperature and high pressure', isCorrect: false),
-        Option(opt: 'low temperature and low pressure', isCorrect: false),
+        Option(txt: 'high temperature', isCorrect: false),
+        Option(txt: 'high pressure', isCorrect: true),
+        Option(txt: 'high temperature and high pressure', isCorrect: false),
+        Option(txt: 'low temperature and low pressure', isCorrect: false),
       ]),
   Question(
       txt:
           'How many chloride ions are surrounding sodium ion in sodium chloride crystal ?',
       options: [
-        Option(opt: '4', isCorrect: false),
-        Option(opt: '8', isCorrect: false),
-        Option(opt: '12', isCorrect: false),
-        Option(opt: '6', isCorrect: true),
+        Option(txt: '4', isCorrect: false),
+        Option(txt: '8', isCorrect: false),
+        Option(txt: '12', isCorrect: false),
+        Option(txt: '6', isCorrect: true),
       ]),
   Question(txt: 'In NaCl structure', options: [
-    Option(opt: 'only octahedral sites are occupied', isCorrect: true),
+    Option(txt: 'only octahedral sites are occupied', isCorrect: true),
     Option(
-        opt: 'all octahedral and tetrahedral sites are occupied',
+        txt: 'all octahedral and tetrahedral sites are occupied',
         isCorrect: false),
-    Option(opt: 'only tetrahedral sites are occupied', isCorrect: false),
+    Option(txt: 'only tetrahedral sites are occupied', isCorrect: false),
     Option(
-        opt: 'neither octahedral nor tetrahedral sites are occupied',
+        txt: 'neither octahedral nor tetrahedral sites are occupied',
         isCorrect: false),
   ]),
   Question(txt: 'In Zinc blende structure', options: [
-    Option(opt: 'it has fee structure', isCorrect: false),
-    Option(opt: 'each S2- ion is surrounded by six Zn2+ ions', isCorrect: true),
+    Option(txt: 'it has fee structure', isCorrect: false),
+    Option(txt: 'each S2- ion is surrounded by six Zn2+ ions', isCorrect: true),
     Option(
-        opt: 'zinc ions occupy half of the tetrahedral sites',
+        txt: 'zinc ions occupy half of the tetrahedral sites',
         isCorrect: false),
     Option(
-        opt: 'each Zn2- ion is surrounded by six sulphide ions',
+        txt: 'each Zn2- ion is surrounded by six sulphide ions',
         isCorrect: false),
   ]),
   Question(
       txt: 'A unit cell of BaCl2 (fluorite structure) is made up of',
       options: [
-        Option(opt: 'eight Ba² ions and four Cl- ions', isCorrect: false),
-        Option(opt: 'four Ba² ions and six Cl- ions', isCorrect: false),
-        Option(opt: 'four Ba2- ions and eight Cl- ions', isCorrect: true),
-        Option(opt: 'four Ba2+ ions and four Cl- ions', isCorrect: false),
+        Option(txt: 'eight Ba² ions and four Cl- ions', isCorrect: false),
+        Option(txt: 'four Ba² ions and six Cl- ions', isCorrect: false),
+        Option(txt: 'four Ba2- ions and eight Cl- ions', isCorrect: true),
+        Option(txt: 'four Ba2+ ions and four Cl- ions', isCorrect: false),
       ]),
   Question(txt: 'Alkali halids do not show Frenkel defect because', options: [
-    Option(opt: 'cations and anions have almost equal size', isCorrect: true),
+    Option(txt: 'cations and anions have almost equal size', isCorrect: true),
     Option(
-        opt: 'there is a large difference in size of cations and anions',
+        txt: 'there is a large difference in size of cations and anions',
         isCorrect: false),
     Option(
-        opt: 'cations and anions have low coordination number',
+        txt: 'cations and anions have low coordination number',
         isCorrect: false),
-    Option(opt: 'anions cannot be accommodated in voids', isCorrect: false),
+    Option(txt: 'anions cannot be accommodated in voids', isCorrect: false),
   ]),
   Question(
       txt: 'The coordination number of Y will be in the XY types of crystal:',
       options: [
-        Option(opt: '8', isCorrect: false),
-        Option(opt: '12', isCorrect: false),
-        Option(opt: '4', isCorrect: false),
-        Option(opt: '6', isCorrect: true),
+        Option(txt: '8', isCorrect: false),
+        Option(txt: '12', isCorrect: false),
+        Option(txt: '4', isCorrect: false),
+        Option(txt: '6', isCorrect: true),
       ]),
   Question(
       txt:
           'Edge length of unit cell of chromium metal is 287 pm with bcc arrangement. The atomic radius is of the order',
       options: [
-        Option(opt: '287 pm', isCorrect: false),
-        Option(opt: '574 pm', isCorrect: false),
-        Option(opt: '124.27 pm', isCorrect: true),
-        Option(opt: '143.5 pm', isCorrect: false),
+        Option(txt: '287 pm', isCorrect: false),
+        Option(txt: '574 pm', isCorrect: false),
+        Option(txt: '124.27 pm', isCorrect: true),
+        Option(txt: '143.5 pm', isCorrect: false),
       ]),
   Question(
       txt:
           'The edge length of fee cell is 508 pm. If radius of cation is 110 pm, the radius of anion is',
       options: [
-        Option(opt: '110 pm', isCorrect: false),
-        Option(opt: '220 pm', isCorrect: false),
-        Option(opt: '285 pm', isCorrect: false),
-        Option(opt: '144 pm', isCorrect: true),
+        Option(txt: '110 pm', isCorrect: false),
+        Option(txt: '220 pm', isCorrect: false),
+        Option(txt: '285 pm', isCorrect: false),
+        Option(txt: '144 pm', isCorrect: true),
       ]),
 ];
